854d9f4fe27a7f63139e21b1669c114a4edd92c7
elkros.prostor@gmail.com <elkros.prostor@gmail.com>
2021-05-26 14:39:37 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UserCollection myCollection = new UserCollection();
+
+            foreach(Element element in myCollection) 
+            {
+                Console.WriteLine("Name: {0} Field1: {1} Field2: {2}", element.Name, element.Field1, element.Field2);
+            }
+
+            //myCollection.Reset();
+
+            Console.Write(new string('-', 29) + "\n");
+
+            foreach(Element element in myCollection)
+            {
+                Console.WriteLine("Name: {0} Field1: {1} Field2: {2}", element.Name, element.Field1, element.Field2);
+            }
+            Console.Write(new string('-', 29) + "\n");
+
+
+
+            /****************************************/
+            // Так работает foreach
+
+            UserCollection myElementCollection = new UserCollection();
+            IEnumerable enumerable = myElementCollection as IEnumerable;
+            IEnumerator enumerator = enumerable.GetEnumerator();
+
+            while (enumerator.MoveNext())
+            {
+                Element element = enumerator.Current as Element;
+
+                Console.WriteLine("Name: {0} Field1: {1} Field2: {2}", element.Name, element.Field1, element.Field2);
+            }
+
+            enumerator.Reset();
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ ConsoleApp2/UserCollection.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+
+namespace ConsoleApp2
+{
+    class UserCollection : IEnumerable, IEnumerator
+    {
+        public Element[] elementsArray = null;
+
+        public UserCollection()
+        {
+            elementsArray = new Element[4];
+            elementsArray[0] = new Element("A", 1, 10);
+            elementsArray[0] = new Element("B", 2, 20);
+            elementsArray[0] = new Element("C", 3, 30);
+            elementsArray[0] = new Element("D", 4, 40);
+        }
+
+        // указатель текущей позиции элемента в массиве
+        int position = -1;
+
+        // реализация интерфейса IEnumerator
+
+        // Передвинуть внутренний указатель position на одну позицию
+        public bool MoveNext()
+        {
+            if (position < elementsArray.Length - 1)
+            {
+                position++;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        // сбрасываем позицию, устанавливаем перед началом набора
+        public void Reset()
+        {
+            position = -1;
+        }
+
+        // возвращает текущий элемент набора
+        public object Current
+        {
+            get { return elementsArray[position]; }
+        }
+
+        // реализация интерфейса IEnumerable
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this as IEnumerator;
+        }
+    }
+}
