43607f3ee2823842d2990ccd00900d89ca2517ad
elkros.prostor@gmail.com <elkros.prostor@gmail.com>
2021-05-26 15:08:23 +0300

    пример 2

---
+++ 001/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IEnumerable enumerl = UserCollection.Power();
+            foreach(string element in enumerl)
+            {
+                Console.WriteLine(element);
+            }
+            Console.WriteLine(new string('-', 12));
+
+
+            IEnumerable enumerable = UserCollection.Power();
+            IEnumerator enumerator = enumerable.GetEnumerator();
+            while (enumerator.MoveNext())
+            {
+                String element = enumerator.Current as String;
+
+                Console.WriteLine(element);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 001/UserCollection.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _001
+{
+    class UserCollection
+    {
+        public static IEnumerable Power()
+        {
+            yield return "Hello world!";
+        }
+    }
+}
