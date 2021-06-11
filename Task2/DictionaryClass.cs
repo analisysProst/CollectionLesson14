5ccc2b3a00e3e076505b6b377f5ac5525a7831f6
elkros.prostor@gmail.com <elkros.prostor@gmail.com>
2021-06-11 16:12:22 +0300

    Add DictionaryClass

---
+++ Task2/DictionaryClass.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class DictionaryClass<TKey, TValue> : IEnumerable<object>, IEnumerator<object>
+    {
+        public TKey[] key = null;
+        public TValue[] value = null;
+        public int position = -1;
+        public int temp = 0;
+                
+        //// Конструктор
+        //public DictionaryClass(int n)
+        //{
+        //    key = new TKey[n];
+        //    value = new TValue[n];
+        //    lenght = n;
+        //}
+        
+        /// Индексатор
+        public string this[int index]
+        {
+            get
+            {
+                if (index >= 0 && index < key.Length)
+                    return key[index] + " - " + value[index];
+                return "Попытка обращения за пределы массива.";
+            }
+        }
+
+        // Метод добавления пары ключ-значение в коллекцию
+        //public void Add(int i, TKey k, TValue l)
+        //{
+        //    key[i] = k;
+        //    value[i] = l;
+        //}
+        
+        /// Метод добавления пары ключ-значение в коллекцию
+        public void Add(TKey k, TValue v)
+        {
+            this.temp++;
+
+            Array.Resize(ref key, temp);
+            key[temp - 1] = k;
+            Array.Resize(ref value, temp);
+            value[temp - 1] = v;
+        }
+
+        /// Метод подсчета общего количества элементов в коллекции
+        public int Count
+        {
+            get
+            {
+                if (key == null) { return 0; }
+                else return key.Length;
+            }
+        }
+
+
+        // Реализация интерфейса IEnumerator<T>
+        public bool MoveNext()
+        {
+            if (position < key.Length - 1)
+            {
+                position++;
+                return true;
+            }
+            return false;
+        }
+
+        public void Reset()
+        {
+            position = -1;
+        }
+
+        public object Current
+        {
+            get { return key[position] + " " + value[position]; }
+        }
+
+        object IEnumerator.Current
+        {
+            get { return key[position] + " " + value[position]; }
+        }
+
+        // Реализация интерфейса IEnumerable<T>
+        public IEnumerator<object> GetEnumerator()
+        {
+            return this;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this;
+        }
+
+        public void Dispose()
+        {
+            Reset();
+        }
+    }
+}
--- Task2/Program.cs
+++ Task2/Program.cs
@@ -5,83 +5,33 @@
  * значения элемента по указанному индексу и свойство только для чтения 
  * для получения общего количества элементов.
  Реализуйте возможность перебора элементов коллекции в цикле foreach*/
+
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace Task2
 {
-    //параметризированный класс Т (указатель места заполнения типа)
-    class MyDictionaryClass<TKey, TValue>
-    {
-        public TKey[] keys = null; // массив ключей
-        public TValue[] values = null;// массив значений
-        public int temp = 0;
-
-        // метод для добавления элемента
-        public void Add(TKey key, TValue value)
-        {
-            if(keys == null)
-                keys = new TKey[1];
-
-            if (values == null)
-                values = new TValue[1];
-
-            if (keys.Length == temp)
-                Array.Resize(ref keys, keys.Length + 1);
-            keys[temp] = key;
-
-            if(values.Length == temp)
-                Array.Resize(ref values, values.Length + 1);
-            values[temp] = value;
-
-            this.temp++;
-
-        }
-
-        // индексатор для получения значения элемента по указаному индексу (ключу)
-        // индексаторы друг от друга отличаются типом или количеством индексов
-        public TValue this[TKey key]
-        {
-            get
-            {
-                
-            }
-        }
-
-        //метод для получения общего количества элементов
-        public int Count
-        {
-            get
-            {
-                if (keys == null) { return 0; }
-                else return keys.Length;
-            }
-        }
-
-    }
-
     class Program
     {
         static void Main(string[] args)
         {
-            MyDictionaryClass<int, int> dict = new MyDictionaryClass<int, int>();
-            Console.WriteLine("My dictionary: " + dict.Count);
-            dict.Add(5, 10);
-            Console.WriteLine("My dictionary: " + dict.Count);
-            Console.WriteLine("Element[0]: " + dict[0]);
-            dict.Add(9, 11);
-            Console.WriteLine("My dictionary: " + dict.Count);
-            Console.WriteLine("Element[1]: " + dict[1]);
+            
+            DictionaryClass<int, int> dictionary = new DictionaryClass<int,int>();
+            Console.WriteLine("My dictionary: " + dictionary.Count);
+            dictionary.Add(1, 6);
+            Console.WriteLine("My dictionary: " + dictionary.Count);
+            dictionary.Add(5, 3);
+            Console.WriteLine("My dictionary: " + dictionary.Count);
+            dictionary.Add(2, 4);
+            Console.WriteLine("My dictionary: " + dictionary.Count);
 
-            //// Получить коллекцию ключей
-            //ICollection<int> keys = dict.keys;
+            Console.WriteLine("Element[0]: " + dictionary[0]);
+            Console.WriteLine("Element[1]: " + dictionary[1]);
+            Console.WriteLine("Element[2]: " + dictionary[2]);
 
-            //foreach (int j in keys)
-            //    Console.WriteLine("Key -> {0}  Value -> {1}", j, dict[j]);
 
+
+            Console.ReadKey();
         }
     }
 }
